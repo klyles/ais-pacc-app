de91c5dcb2d9bf755142c2422b8bfc133824670f
Venkatesh Narlakanti <Venkatesh.Narlakanti@teksystemsgs.com>
2017-08-24 15:47:50 -0500

    +apiConfig Service for CORS

---
+++ src/app/shared/services/apiconfig.ts
@@ -0,0 +1,37 @@
+export const BaseHeader = {
+      'Content-Type': 'text/plain'
+};
+export const BaseUrl = ' http://ahwacotxpladbt:5000/api';
+
+/* Use this for PROD environment:
+   export const REST_API_URL = 'https://sb-fhir-dstu2.smarthealthit.org'; */
+
+/* Use this for QA environment:
+  export const REST_API_URL_QA = 'https://sb-fhir-dstu2.smarthealthit.org'; */
+
+
+// export const BaseUrl = getEnv()
+
+// function getEnv() {
+//   let env: string;
+//   let data = {
+//         endPoint: ''
+//   };
+//   console.log('Mode: ' + MODE);
+//   env = MODE;
+
+//   switch (env) {
+//     case 'development':
+//     data = {
+//       endPoint: REST_API_URL_QA
+//     }
+//     break;
+//   };
+//   switch (prod) {
+//     case 'production':
+//     data = {
+//       endPoint: REST_API_URL
+//     }
+//     break;
+//   };
+// }
