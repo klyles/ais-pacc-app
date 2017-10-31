0385692e3859af039ebfb82e9115f889c0d52282
Venkatesh Narlakanti <Venkatesh.Narlakanti@teksystemsgs.com>
2017-10-31 15:18:26 -0500

    SSO

--- src/app/+login/login.component.ts
+++ src/app/+login/login.component.ts
@@ -13,10 +13,10 @@ export class LoginComponent implements OnInit {
   ssoHTML: any;
   singleSignOnToken: any;
   isSignOn: boolean;
-  constructor(private router: Router, private LoginService: LoginService) { }
+  constructor(private router: Router, private _loginService: LoginService) { }
 
   ngOnInit() {
-  // retrive the previousUrl from route params or default to '/'
+  // retrieve the previousUrl from route params or default to '/'
     this.isSignOn = false;
     const prevUrl = localStorage.getItem('prevUrl');
     this.returnUrl = prevUrl ? prevUrl : '/dashboard/analytics';
@@ -24,10 +24,21 @@ export class LoginComponent implements OnInit {
     this.getIcVersion();
     this.getIcSSO();
     this.ssoTimeOut();
+    // this.reDirectUri();
+    // this.getSSOReturn();
     // this.postSamlRequest();
   }
+  reDirectUri() {
+    window.open('https://ciccrm.ascension.org/api/ahwivrtxpla001.ds.sjhs.com/icws/connection/single-sign-on' +
+    '/identity-providers/' + this.idProviders + '?' +
+    'singleSignOnCapabilities=saml2Post%2Csaml2Redirect&redirectUri=https%3A%2F%2Fwww.google.com');
+
+    console.log('navigating to: https://www.google.com');
+  //  this.getSSOResponse();
+    this.responseTimeOut();
+  }
   getIcFeatures() {
-    this.LoginService.getIcFeatures()
+    this._loginService.getIcFeatures()
     .subscribe(
       (response: any) => {
         console.log(response);
@@ -35,7 +46,7 @@ export class LoginComponent implements OnInit {
     )
   }
   getIcVersion() {
-    this.LoginService.getIcVersion()
+    this._loginService.getIcVersion()
     .subscribe(
       (response: any) => {
         console.log(response);
@@ -43,27 +54,31 @@ export class LoginComponent implements OnInit {
     )
   }
   getIcSSO() {
-    this.LoginService.getIcSSO()
+    this._loginService.getIcSSO()
     .subscribe(
       (response: any) => {
         this.idProviders = response.authentication.identityProviders[0].identityProviderId;
       }
     )
+    // this.reDirectUri();
   }
   public ssoTimeOut() {
     setTimeout(() => this.getIcSSOProviders(), 1000);
   }
   getIcSSOProviders() {
-    this.LoginService.getSSOidProviders(this.idProviders)
+    this._loginService.getSSOidProviders(this.idProviders)
     .subscribe(
       (response: any) => {
         console.log(this.ssoHTML);
       }
     )
-    this.getSSOResponse();
+   this.reDirectUri();
+  }
+  public responseTimeOut() {
+    setTimeout(() => this.getSSOResponse(), 5000);
   }
   getSSOResponse() {
-    this.LoginService.getSSOResponse()
+    this._loginService.getSSOResponse()
     .subscribe(
       (response: any) => {
         this.singleSignOnToken = response.singleSignOnToken;
@@ -72,8 +87,17 @@ export class LoginComponent implements OnInit {
     )
   }
   postSamlRequest() {
-    this.LoginService.postSamlRequest()
+    this._loginService.postSamlRequest()
   }
+  getSSOReturn() {
+    this._loginService.getSSOReturn()
+    .subscribe(
+      (response: any) => {
+        console.log(response);
+      }
+    )
+  }
+
   onSubmit() {
     this.form = {
       '__type': 'urn:inin.com:connection:singleSignOnTokenConnectionRequestSettings',
@@ -82,27 +106,9 @@ export class LoginComponent implements OnInit {
       // 'SAMLRequest' : 'PHNhbWxwOkF1dGhuUmVxdWVzdCAgICB4bWxuczpzYW1sPSJ1cm46b2FzaXM6bmFtZXM6dGM6U0FNTDoyLjA6YXNzZXJ0aW9uIiAgICB4bWxuczpzYW1scD0idXJuOm9hc2lzOm5hbWVzOnRjOlNBTUw6Mi4wOnByb3RvY29sIiAgICBJRD0iX2YzNWY2ZTQ5MzMzYjM1MTc5MTQxZWEwODI5YjY1MDIwIiAgICBWZXJzaW9uPSIyLjAiICAgIElzc3VlSW5zdGFudD0iMjAxNy0xMC0yNFQxNjo0MzoxNy43MTEzNDdaIiAgICBEZXN0aW5hdGlvbj0iaHR0cHM6Ly9jaWNjcm0uYXNjZW5zaW9uLm9yZy9hcGkvYWh3aXZydHhwbGEwMDEuZHMuc2pocy5jb20vaWN3cy9jb25uZWN0aW9uL3NpbmdsZS1zaWduLW9uL2F1dGhlbnRpY2F0aW9uL1dpbmRvd3NJRFAvbG9naW4',
       // 'RelayState' : '90c22f9b-0a9d-4474-87f4-b48eccbe3095|r:https://ciccrm.ascension.org/#/single-sign-on/ahwivrtxpla001.ds.sjhs.com/ahwivrtxpla001.ds.sjhs.com'
     }
-    this.LoginService.onsubmit(this.form);
+    this._loginService.onsubmit(this.form)
     alert('login successful! and navigating to ' + this.returnUrl + ' ');
     this.router.navigate([this.returnUrl]);
     this.isSignOn = true;
   }
-
-  // private setCookie(name: string, value: string, expireDays: number, path: string = '') {
-  //   const d: Date = new Date();
-  //   d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
-  //   const expires = `expires=${d.toUTCString()}`;
-  //   const cpath: string = path ? `; path=${path}` : '';
-  //   document.cookie = `${name}=${value}; ${expires}${cpath}`;
-  // }
-  // public fetchCookie() {
-  //   const path = 'accesion_CRM-Lite_internal';
-  //   const expiry = new Date();
-  //   const name = this.form.username;
-  //   const password = this.form.password;
-  //   const init_sso = this.isSignOn;
-  //   document.cookie = 'CRM_seton=' + path + ';' + expiry + ';'
-  //   + name + ';' + password + ';' + init_sso;
-  //   console.log('setCookie');
-  // }
 }
--- src/app/+login/login.service.ts
+++ src/app/+login/login.service.ts
@@ -1,6 +1,6 @@
 import { APIServices } from './../shared/services/api.services';
 import { Injectable } from '@angular/core';
-import { Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
@@ -8,7 +8,7 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 
 
 @Injectable()
-export class LoginService  extends APIServices {
+export class LoginService extends APIServices {
   isLoggedIn: boolean = false;
   host = 'testing host';
 
@@ -26,17 +26,10 @@ export class LoginService  extends APIServices {
     const url = this.baseICUrl;
     console.log(url);
     const headers = new Headers({
-      'Accept-Language' : 'en-US',
-      // 'Access-Control-Allow-Origin' : '*',
-     // 'Host' : 'ciccrm.ascension.org',
-     // 'Origin' : 'https://ciccrm.ascension.org',
-     // 'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-      // 'Access-Control-Allow-Methods' : 'HEAD, GET, POST, PUT, PATCH, DELETE',
-      // 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-      // 'Referer' : 'https://ciccrm.ascension.org/api/ahwivrtxpla001.ds.sjhs.com/icws/connection/single-sign-on/identity-providers/90c22f9b-0a9d-4474-87f4-b4'
+      'Accept-Language': 'en-US'
     })
-    const options = new RequestOptions({headers: headers});
-   // return this._http.post(url, credentials, options).map(res => res.json()).subscribe();
+    const options = new RequestOptions({ headers: headers });
+    // return this._http.post(url, credentials, options).map(res => res.json()).subscribe();
     // const url =  this.baseUrl + '/account/auth2';
     // const body = 'username=' + credentials.username + '&password=' + credentials.password;
     return this._http.post(url, form, options).map(res => res.json()).subscribe();
@@ -44,37 +37,42 @@ export class LoginService  extends APIServices {
   getIcFeatures(): Observable<any> {
     const url = this.baseICUrl + '/features';
     console.log(url);
-    return this._http.get(url, this.options).map((response: any) => {return response.json(); }).catch(this.handleError.bind(this));
+    return this._http.get(url, this.options).map((response: any) => { return response.json(); }).catch(this.handleError.bind(this));
+  }
+  getSSOReturn(): Observable<any> {
+    const url = this.baseICUrl + '/single-sign-on/return';
+    console.log(url);
+    return this._http.get(url, this.options).map((response: any) => { return response.json(); }).catch(this.handleError.bind(this));
   }
   getIcVersion(): Observable<any> {
     const url = this.baseICUrl + '/version';
     console.log(url);
-    return this._http.get(url, this.options).map((response: any) => {return response.json(); }).catch(this.handleError.bind(this));
+    return this._http.get(url, this.options).map((response: any) => { return response.json(); }).catch(this.handleError.bind(this));
   }
   getIcSSO(): Observable<any> {
     const url = this.baseICUrl + '/server-info?singleSignOnCapabilities=saml2Post,saml2Redirect';
     console.log(url);
-    return this._http.get(url, this.options).map((response: any) => {return response.json(); }).catch(this.handleError.bind(this));
+    return this._http.get(url, this.options).map((response: any) => { return response.json(); }).catch(this.handleError.bind(this));
   }
   getSSOResponse(): Observable<any> {
     const url = this.baseICUrl + '/single-sign-on/response';
     console.log(url);
-    return this._http.get(url, this.options).map((response: any) => {return response.json(); }).catch(this.handleError.bind(this));
+    return this._http.get(url, this.options).map((response: any) => { return response.json(); }).catch(this.handleError.bind(this));
   }
-
   getSSOidProviders(id_provider): Observable<any> {
     const url = this.baseICUrl + '/single-sign-on/identity-providers/' + id_provider + '?singleSignOnCapabilities=' +
-    'saml2Post%2Csaml2Redirect&' +
-    'redirectUri=http://localhost:4200/#/login';
-    // 'redirectUri=https%3A%2F%2Fciccrm.ascension.org%2F%23%2Fsingle-sign-on%2Fahwivrtxpla001.ds.sjhs.com%2Fahwivrtxpla001.ds.sjhs.com';
+      'saml2Post%2Csaml2Redirect&' +
+      'redirectUri=https://www.google.com';
+    // 'redirectUri=http%3A%2F%2Flocalhost%3A4200%2Flogin';
+    // 'redirectUri=https%3A%2F%2Fciccrm.ascension.org%2F%23%2Fsingle-sign-on%2Fahwivrtxpla001.ds.sjhs.com';
     console.log(url);
-    return this._http.get(url, this.options).map((response: any) => {return response.json(); }).catch(this.handleError.bind(this));
+    return this._http.get(url, this.options).map((response: any) => { return response.json(); }).catch(this.handleError.bind(this));
   }
   postSamlRequest() {
     const url = this.baseICUrl + '/features';
     console.log(url);
     const headers = new Headers({ 'Content-Type': 'application/json', 'Accept': 'application/json' });
-    const options = new RequestOptions({headers: headers});
+    const options = new RequestOptions({ headers: headers });
     return this._http.post(url, options).map(res => res.json()).subscribe();
   }
 }
